87a619e6f4906e4697e05b10681cb2ad8c0266d5
Steve Robbins <steven.j.robbins@gmail.com>
2015-01-14 15:31:35 -0800

    better 5.4

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
 
-cd /tmp/
-curl -o epel-release-7-5.noarch.rpm http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-yum -y install epel-release-7-5.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 
-yum -y update
 yum -y install \
   httpd \
-  mariadb-server mariadb \
-  php \
-  php-curl \
-  php-devel \
-  php-gd \
-  php-intl \
-  php-pear \
-  php-imap \
-  php-mcrypt \
-  php-mysql \
-  php-pdo \
-  php-pspell \
-  php-recode \
-  php-snmp \
-  php-tidy \
-  php-xmlrpc \
-  php-xsl \
+  mysql-server \
+  php54w \
+  php54w-curl \
+  php54w-devel \
+  php54w-gd \
+  php54w-intl \
+  php54w-pear \
+  php54w-imap \
+  php54w-mcrypt \
+  php54w-mysql \
+  php54w-pdo \
+  php54w-pspell \
+  php54w-recode \
+  php54w-snmp \
+  php54w-tidy \
+  php54w-xmlrpc \
+  php54w-xsl \
   vim
 
 echo '<VirtualHost *:80>
@@ -33,16 +30,15 @@ echo '<VirtualHost *:80>
     Order allow,deny
     Allow from all
   </Directory>
-
   Options +ExecCGI +FollowSymLinks -Indexes -MultiViews
   DirectoryIndex index.php index.html index.htm
   UseCanonicalName off
   AddType application/x-httpd-php .php
   ServerName v0.steverobbins.name
-
   <IfModule mod_rewrite.c>
     RewriteEngine On
-
+    RewriteLog "/var/log/httpd/rewrite.log"
+    RewriteLogLevel 0
     RewriteCond   %{HTTP_HOST}                         ^[^.]+\.v0\.steverobbins\.name$
     RewriteRule   ^(.+)                                %{HTTP_HOST}$1                  [C]
     RewriteRule   ^([^.]+)\.v0\.steverobbins\.name(.*) /var/www/html/$1/$2             [L]
@@ -66,33 +62,56 @@ xdebug.profiler_enable_trigger=1
 zend_extension=/usr/lib64/php/modules/xdebug.so' >> /etc/php.ini
 
 service httpd start
-service mariadb start
+service mysqld start
+
+mysql -e "
+CREATE USER 'root'@'%' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+"
 
 cd ~
 
 curl -sS https://getcomposer.org/installer | php
 chmod +x composer.phar
+mv composer.phar /usr/local/bin/composer
 
 git clone https://github.com/netz98/n98-magerun.git
 cd n98-magerun
-php ~/composer.phar install
+composer install
 cd /usr/local/bin
 ln -s ~/n98-magerun/bin/n98-magerun magerun
-
-mv ~/composer.phar /usr/local/bin/composer
 
 echo '
 alias ls="ls -ahF --color=auto"
 alias ll="ls -l"
 alias grep="grep --color=auto"
-
+dynmotd () {
+  local NONE="\033[0m"    
+  # regular colors
+  local K="\033[0;30m"    # black
+  local R="\033[0;31m"    # red
+  local G="\033[0;32m"    # green
+  local Y="\033[0;33m"    # yellow
+  local B="\033[0;34m"    # blue
+  local M="\033[0;35m"    # magenta
+  local C="\033[0;36m"    # cyan
+  local W="\033[0;37m"    # white
+  local PROCCOUNT=`ps -l | wc -l`
+  local PROCCOUNT=`expr $PROCCOUNT - 4`
+  echo -e "${W}+------------------------------------------------------------------------------+
+| ${W}Hostname:$C       `hostname`
+${W}| ${W}IP Address:$C     `ifconfig | grep -Eo \"inet (addr:)?([0-9]*\.){3}[0-9]*\" | grep -Eo \"([0-9]*\.){3}[0-9]*\" | grep -v \"127.0.0.1\" | xargs`
+${W}| ${W}Uptime:$C         `uptime | sed \"s/.*up \([^,]*\), .*/\1/\" | xargs`
+${W}| ${W}Sessions:$C       `who | grep $USER | wc -l | xargs`
+${W}| ${W}Processes:$C      $PROCCOUNT$NONE"
+}
+dynmotd
+unset dynmotd
 __cur_dir() {
     pwd
 }
-
 bash_prompt() {
   local NONE="\[\033[0m\]"    
-
   # regular colors
   local K="\[\033[0;30m\]"    # black
   local R="\[\033[0;31m\]"    # red
@@ -102,10 +121,8 @@ bash_prompt() {
   local M="\[\033[0;35m\]"    # magenta
   local C="\[\033[0;36m\]"    # cyan
   local W="\[\033[0;37m\]"    # white
-
   local UC=$W                 
   [ $UID -eq "0" ] && UC=$R   
-
   PS1="${W}"
   USERHOST="$USER@$HOSTNAME"
   CNT=$(echo $USERHOST | wc -m)
@@ -115,23 +132,6 @@ bash_prompt() {
   PS1="$PS1\n${W}| $G\$(__cur_dir)$NONE"
   PS1="$PS1\n$NONE\\$ "
 }
-
 bash_prompt
 unset bash_prompt
 ' >> /etc/bashrc
-
-echo '[client]
-user=root
-password=root' >> ~/.my.cnf
-
-echo "mysql_secure_installation
-
-mysql -e \"
-CREATE USER 'root'@'%' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-\"" >> ~/setup_mysql.sh
-
-chmod +x ~/setup_mysql.sh
-
-cd ~
-ln -s /var/www/html
